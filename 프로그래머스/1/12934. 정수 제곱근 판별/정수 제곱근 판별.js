be5b0e94ef4d45c879c34495690a7873cc0eb5f6
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-03-12 23:02:00 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,9 +1,6 @@
 function solution(n) {
     var answer = 0;
     const isInteger = (Number.isInteger(Math.sqrt(n)));
-    if(isInteger){
-       answer = Math.sqrt(n)+1 ** 2;
-    }
     answer = isInteger ? (Math.sqrt(n)+1) ** 2 : -1;
     return answer;
 }
