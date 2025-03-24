7ad93e2b05a7292c594cfb257c77e65a2b86c208
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-03-24 22:40:03 +0900

    [level 2] Title: 숫자의 표현, Time: 2.41 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var answer = 1;
+    for(let i=1; i<=n/2; i++){
+        var sum = 0;
+        var temp = i;
+        while(sum < n){
+            sum += temp
+            temp++;
+        }
+        if(sum == n) answer+=1;
+    };
+    return answer;
+}
