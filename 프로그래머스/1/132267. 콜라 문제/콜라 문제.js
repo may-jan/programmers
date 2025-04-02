65ba1a0cc90e1b44f2a4b1e328b0792ec46eb3cb
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-04-02 21:14:10 +0900

    [level 1] Title: 콜라 문제, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,11 @@
+function solution(a, b, n) {
+    var answer = 0;
+    let bottle = n;  
+    while(bottle >= a){
+        let minus = Math.floor(bottle / a) * a;
+        let newBottles = Math.floor(bottle / a) * b;
+        answer += newBottles;
+        bottle = newBottles + (bottle % a);
+    }    
+    return answer;
+}
