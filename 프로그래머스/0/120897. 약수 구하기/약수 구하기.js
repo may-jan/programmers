5031744971659597de08616904304a860eb443e7
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-04-16 20:07:48 +0900

    [level 0] Title: 약수 구하기, Time: 0.75 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -2,7 +2,7 @@ function solution(n) {
     var answer = [];
     var temp = n;
     for(let i=1; i<=n; i++){
-        if(n%i == 0) answer.unshift(n/i); 
+        if(n%i == 0) answer.push(i); 
         temp = n/i;
     };  
     return answer;
