807203b8c0f25c7b5f1c1d832305acb9117fdd6d
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-05-13 14:55:46 +0900

    [level 0] Title: l로 만들기, Time: 6.09 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.js
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.js
@@ -1,8 +1,3 @@
 function solution(myString) {
-    var answer = '';
-    for(let i=0; i<myString.length; i++){
-        myString[i] < "l" ?
-            answer += "l" : answer += myString[i];
-    };
-    return answer;
+    return myString.replaceAll(/[a-k]/g, 'l');
 }
