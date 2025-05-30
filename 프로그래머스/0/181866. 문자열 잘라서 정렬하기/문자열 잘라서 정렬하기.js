ead7b4456f4d4f03075cce443549f7fd34e80d43
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-05-30 16:23:24 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 13.22 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.js
@@ -1,5 +1,5 @@
 function solution(myString) {
     const changeArr = myString.split("x").filter((str)=>str.length!=0);
-    const sortedArr = changeArr.sort((a,b)=>a.localeCompare(b));
+    const sortedArr = changeArr.sort();
     return sortedArr;
 }
