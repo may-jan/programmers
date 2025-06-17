d8cf6c9d63e18c5809695a65a3f18a06d3b8c5cf
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-06-17 16:31:12 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,33 +1,6 @@
 function solution(lottos, win_nums) {
-    var answer = [];
-    let cnt = 0; 
-    let zero = 0;
-    for(let i=0; i<win_nums.length; i++){
-        if(lottos.includes(win_nums[i])) cnt+=1;
-        if(lottos[i] == 0) zero+=1;
-    }
-    for(let i=0; i<2; i++){
-        let match = i == 0 ? cnt + zero : cnt;
-        switch(match){
-            case 6:
-                answer.push(1)
-                break;
-            case 5 :
-                answer.push(2)
-                break;
-            case 4 :
-                answer.push(3)
-                break;
-            case 3 :
-                answer.push(4)
-                break;
-            case 2 :
-                answer.push(5)
-                break;
-            default :
-                answer.push(6)
-                break;
-        }
-    }
-    return answer;
+    const rank = [6, 6, 5, 4, 3, 2, 1];    
+    const match = lottos.filter((n)=>win_nums.includes(n)).length;
+    const zero = lottos.filter((n)=>n==0).length;
+    return [rank[match+zero], rank[match]];
 }
