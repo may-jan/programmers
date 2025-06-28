51f66260657a5af31e51c4e85b3c4076f1d20b80
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-06-28 15:49:10 +0900

    [level 1] Title: 체육복, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,19 +1,14 @@
 function solution(n, lost, reserve) {
-    var answer = n - lost.length;
-    let realLost = lost.filter((x)=>!reserve.includes(x)).sort((a,b)=>a-b);
-    let realReserve = reserve.filter((x)=>!lost.includes(x)).sort((a,b)=>a-b);
+    let realLost = lost.filter((x) => !reserve.includes(x)).sort((a, b) => a - b);
+    let realReserve = reserve.filter((x) => !lost.includes(x)).sort((a, b) => a - b);
+    
     for(let i=0; i<realReserve.length; i++){
         if(realLost.includes(realReserve[i] - 1)){
-            // const idx = realLost.indexOf(realReserve[i]-1);
-            realLost.splice(realLost.indexOf(realReserve[i] - 1), 1)
-            // realLost[idx] = "X";
-            answer ++;
+            realLost.splice(realLost.indexOf(realReserve[i] - 1), 1);
         }else if(realLost.includes(realReserve[i] + 1)){
-            // const idx = realLost.indexOf(realReserve[i]+1);
-            realLost.splice(realLost.indexOf(realReserve[i] + 1), 1)
-            // realLost[idx] = "X";
-            answer ++;
+            realLost.splice(realLost.indexOf(realReserve[i] + 1), 1);
         }
     }
-    return n-realLost.length;
+    
+    return n - realLost.length;
 }
