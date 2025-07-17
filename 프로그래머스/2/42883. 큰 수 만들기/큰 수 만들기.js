c41659e5915364e252743e0efcb397e1d3021387
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-07-17 20:41:11 +0900

    [level 2] Title: 큰 수 만들기, Time: 31.96 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,13 @@
+function solution(number, k) {
+    const stack = [];
+    for(let i=0; i<number.length; i++){
+        const num = number[i];      
+        while(k>0 && stack.length>0 && stack[stack.length - 1]<num){
+            stack.pop();
+            k--;
+        }        
+        stack.push(num);
+    }
+
+    return stack.slice(0, stack.length-k).join('');
+}
