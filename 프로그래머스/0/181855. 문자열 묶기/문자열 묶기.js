9f19a047381905326f1755d96f4da9cce4709ae6
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-07-18 23:11:00 +0900

    [level 0] Title: 문자열 묶기, Time: 20.64 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.js
@@ -0,0 +1,8 @@
+function solution(strArr) {
+    const map = new Map();
+    strArr.forEach((str) => {
+        map.set(str.length, (map.get(str.length) || 0) + 1)
+    })
+    return Math.max(...map.values());
+    
+}
