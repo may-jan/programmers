22794cb3cfacae0598496e474400ebacdd99346e
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-07-19 22:39:42 +0900

    [level 0] Title: A로 B 만들기, Time: 0.69 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,14 +1,6 @@
 function solution(before, after) {
-    var answer = 1;
-    const map = new Map();
-    for(let i=0; i<before.length; i++){
-        map.set(before[i], (map.get(before[i])||0) + 1)
-    }
-    for(let i=0; i<after.length; i++){
-        map.set(after[i], (map.get(after[i])||0) - 1)
-    }    
-    map.forEach((value, key) => {
-        if(value !== 0) answer = 0
-    })
-    return answer;
+    const splitedBe = before.split("").sort().join("");
+    const splitedAf = after.split("").sort().join("");
+    
+    return splitedBe == splitedAf ? 1 : 0;
 }
