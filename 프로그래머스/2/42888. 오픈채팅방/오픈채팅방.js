c1db73ee6c1c02de1c5fca36be5e87074c875fa1
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-08-16 17:08:32 +0900

    [level 2] Title: 오픈채팅방, Time: 164.49 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -3,18 +3,14 @@ function solution(record) {
     const userMap = new Map();
     const splitedRecord = record.map(r => r.split(" "));
     splitedRecord.forEach(r => {
-        if(r[0] == 'Enter' || r[0] == "Change"){
-            userMap.set(r[1], r[2])
-        }
+        if(r[0] == 'Enter' || r[0] == "Change") userMap.set(r[1], r[2]);
     });
     splitedRecord.forEach(r => {
         if(r[0] == 'Enter'){
-            const text = `${userMap.get(r[1])}님이 들어왔습니다.`;
-            answer.push(text)
+            answer.push(`${userMap.get(r[1])}님이 들어왔습니다.`);
         }
         if(r[0] == "Leave"){
-            const text = `${userMap.get(r[1])}님이 나갔습니다.`;
-            answer.push(text)
+            answer.push(`${userMap.get(r[1])}님이 나갔습니다.`);
         }
     })
     return answer;
