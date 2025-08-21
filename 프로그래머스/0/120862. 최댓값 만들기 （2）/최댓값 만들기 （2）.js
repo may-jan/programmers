95d7ec9a7fbe0f823e444d2bd7bb73b55343d1e9
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-08-21 23:37:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,10 +1,5 @@
 function solution(numbers) {
-var answer = [];
-    for(let i = 0; i < numbers.length - 1; i++){
-        for(let j = i + 1; j < numbers.length; j++){
-            answer.push(numbers[i] * numbers[j]);
-        }
-    }
-    return Math.max(...answer);
+    const len = numbers.length;
+    numbers.sort((a,b) => a-b);
+    return Math.max(numbers[0] * numbers[1], numbers[len-1] * numbers[len-2]);
 }
-
