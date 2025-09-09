b7b626a13418c47ba4a7dfacdfd288c1c356d2d8
Jaeeun <116541435+may-jan@users.noreply.github.com>
2025-09-09 17:25:35 +0900

    [level 0] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,13 @@
+function solution(n, slicer, num_list) {
+    const [a, b, c] = slicer;
+    if(n == 1) return num_list.slice(0, b+1);
+    if(n == 2) return num_list.slice(a);
+    if(n == 3) return num_list.slice(a, b+1);
+    if(n == 4){
+        let res = [];
+        for(let i=a; i<=b; i+=c) {
+            res.push(num_list[i]);
+        }
+        return res;
+    }
+}
